90f27abbfb7058ea4dac6bce7faab2467b54bb1c
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-08-22 20:43:10 +0400

    JS

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// actions
+let clear = document.getElementById("clear");
+let percent = document.getElementById("percent");
+let root = document.getElementById("root");
+let division = document.getElementById("division");
+let multiplication = document.getElementById("multiplication");
+let minus = document.getElementById("addition");
+let plus = document.getElementById("subtraction");
+let equality = document.getElementById("equality");
+let comma = document.getElementById("comma");
+
+//numbers
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
